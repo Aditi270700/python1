9ca79165c3beb6e924ed3252ee1c9f0b48e09021
aditi saudagar <aditisaudagar17@gmail.com>
2025-05-30 21:30:18 +0530

    list_function

---
+++ list1_function.py
@@ -0,0 +1,48 @@
+#  List Function
+
+#  count()
+#  max()
+#  min()
+#  sort()
+#  reverse()
+#  index()
+
+l=[10,20,30,10,45,10,50]
+c=l.count(10)
+print(c)
+
+l=[10,20,30,10,45,10,50]
+c=max(l)
+print(c)
+
+l=[10,20,30,85,2,5]
+c=min(l)
+print(c)
+
+
+l=["aditi","hello","ishika","aarohi"]
+c=min(l)
+print(c)
+
+l=["aditi","hello","ishika","aarohi"]
+c=max(l)
+print(c)
+
+# sort means aarange in ascending order
+l=[10,2,5,7,9,10]
+l.sort()
+print(l)
+
+# reverse means arrange all value from the last
+l=[10,52,65,41,20]
+l.reverse()
+print(l)
+
+# index it returns index number of the elements
+l=[10,20,30,40,52,65]
+c=l.index(20)
+print(c)
+
+l=["hello","world"]
+l.reverse()
+print(l)
--- list_comprehension.py
+++ list_comprehension.py
@@ -1,7 +1,9 @@
+# list comprehension
 l =[]
 for a in range(1,101):
     l.append(a)
 print(l)
+
 print("")
 n = [h for h in range(1,101) if h%2==0]
 print(n)
